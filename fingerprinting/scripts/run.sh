9b8657da8810b2374a69c69c00171b681cebc8a7
zhangxin00 <47495105+zhangxin00@users.noreply.github.com>
2026-06-10 12:07:57 +0800

    Update run.sh

--- fingerprinting/scripts/run.sh
+++ fingerprinting/scripts/run.sh
@@ -19,6 +19,9 @@ echo "========================================"
 echo "[0/3] Prepare Python environment"
 echo "========================================"
 
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda activate pitfall-env
+
 # Should use conda
 echo "Python path:"
 which python
